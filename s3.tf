15749584dab5aeec590b9e910b681008e85a76bc
Aviv-Cohen-PAN <166496865+Aviv-Cohen-PAN@users.noreply.github.com>
2025-07-09 17:43:52 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,4 +5,15 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have versioning
   bucket        = "prefix-data"
   force_destroy = true
+  tags = {
+    git_commit           = "16ae402f91674d1653e9482a5ed550ebdfaedebc"
+    git_file             = "s3.tf"
+    git_last_modified_at = "2025-07-09 12:30:57"
+    git_last_modified_by = "166496865+Aviv-Cohen-PAN@users.noreply.github.com"
+    git_modifiers        = "166496865+Aviv-Cohen-PAN"
+    git_org              = "Aviv-Cohen-PAN"
+    git_repo             = "code2cloud-python-flask-webserver"
+    yor_name             = "data"
+    yor_trace            = "b2863525-8553-4f79-948a-b8a991889fca"
+  }
 }
