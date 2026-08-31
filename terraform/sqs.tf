91ece413c1c39b968f0ccdd27a7c327e2f2eff95
RazDarzi <rdarzi@paloaltonetworks.com>
2026-08-31 14:29:01 +0300

    added sqs.tf

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -9,4 +9,9 @@ resource "aws_sqs_queue" "orders" {
   visibility_timeout_seconds = 30
   message_retention_seconds  = 345600 # 4 days
   # fifo_queue = true                  # for orders.fifo
+
+  tags = {
+    "managed_by" = "paloaltonetworks"
+    yor_trace    = "72f38826-b2c9-4e92-a5da-d598cf0a7a2c"
+  }
 }
